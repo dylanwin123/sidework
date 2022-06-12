add187e67de191dfd6662c45713a9ba95412e47d
Dylan Winters <dylanwintersjobs@gmail.com>
2022-06-12 17:01:31 -0400

    start of text based game

---
+++ text game/text game/MainUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text_game
+{
+    public class MainUI
+    {
+        public void ShowMainMenu()
+        {
+            Console.WriteLine("I win");
+        }
+    }
+}
---
+++ text game/text game/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using text_game;
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        MainUI ui = new MainUI();
+        ui.ShowMainMenu();
+    }
+}
+

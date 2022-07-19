0793254d0fb298039012120fb7737b8a6c10c56e
Dylan Winters <dylanwintersjobs@gmail.com>
2022-07-19 15:20:00 -0400

    start of profile page

---
+++ WebApplication1/WebApplication1/Pages/Resume.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class ResumeModel : PageModel
+    {
+        private readonly ILogger<ResumeModel> _logger;
+
+        public ResumeModel(ILogger<ResumeModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
